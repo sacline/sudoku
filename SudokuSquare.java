b345f8e5c26caf2f8807fdb087e0d43c735f14dd
Stephen Cline <stephenacline13@gmail.com>
2016-01-10 13:38:09 -0600

    Changed SudokuSquare pencils from array to ArrayList

--- SudokuSquare.java
+++ SudokuSquare.java
@@ -7,8 +7,11 @@
  * 
  * @version 1.0
  */
+
+import java.util.ArrayList;
+
 public class SudokuSquare {
-  private boolean[] pencils;
+  private ArrayList<Integer> pencils;
   private int value;
 
 /**
@@ -16,10 +19,7 @@ public class SudokuSquare {
  * Pencils set to false, representing no penciled values.
  */
   public SudokuSquare() {
-    pencils = new boolean[9];
-    for (boolean pencil : pencils) {
-      pencil = false;
-    }
+    pencils = new ArrayList<Integer>();
     this.value = 0;
   }
 
@@ -28,11 +28,9 @@ public class SudokuSquare {
  * @param value value to create the square with
  */
   public SudokuSquare(int value) {
-    pencils = new boolean[9];
-    for (boolean pencil : pencils) {
-      pencil = false;
-    }
-      this.value = value;
+    pencils = new ArrayList<Integer>();
+    validateValue(value);
+    this.value = value;
   }
 
 /**
@@ -53,27 +51,27 @@ public class SudokuSquare {
   }
 
 /**
- * Sets a penciled value.
+ * Adds a penciled value.
  * @param value value to pencil in
  */
-  public void setPencil(int value) {
+  public void addPencil(int value) {
     validateValue(value);
-    pencils[value - 1] = true;
+    pencils.add(value);
   }
 
 /**
  * Removes a penciled value.
  * @param value value to unpencil
  */
-  public void unPencil(int value) {
+  public void removePencil(int value) {
     validateValue(value);
-    pencils[value - 1] = false;
+    pencils.remove(new Integer(value));
   }
 
 /**
- * Returns an array of the pencils at the square.
+ * Returns the list of pencils at the square.
  */
-  public boolean[] getPencils() {
+  public ArrayList<Integer> getPencils() {
     return pencils;
   }
 
@@ -81,9 +79,7 @@ public class SudokuSquare {
  * Removes all penciled values.
  */
   public void clearPencils() {
-    for (boolean pencil : pencils) {
-      pencil = false;
-    }
+    pencils.clear();
   }
 
 /**
@@ -105,5 +101,16 @@ public class SudokuSquare {
     if (value < 1 || value > 9) {
       throw new IllegalArgumentException("Value must be between 1 and 9.");
     }
+  }
+
+/**
+ * Method for basic testing.
+ */
+  public static void main(String[] args) {
+    SudokuSquare ss = new SudokuSquare();
+    //ss.setValue(5);
+    //System.out.println(ss.getValue());
+    ss.addPencil(7);
+    System.out.println(ss.getPencils());
   }
 }
