7f96574fc49c3ab5cd5bd1f7606e2a8c7557cdf5
Stephen Cline <stephenacline13@gmail.com>
2016-04-23 17:26:45 -0500

    Fix Google style errors in SudokuBoard.java

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,18 +1,18 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.NoSuchElementException;
 
 /**
  * Standard 9 x 9 sudoku board made from SudokuSquares.
  * SudokuBoard is made of SudokuSquare objects.
- * <p>
- * Cells on the board are specified with a (row, column) in the following way:
+ *
+ * <p>Cells on the board are specified with a (row, column) as follows:
  * (1,1) (1,2) ... (1,9)
  * (2,1) ...
  * .
@@ -24,18 +24,18 @@ public class SudokuBoard {
   private SudokuSquare[][] cells;
   private ArrayDeque<Move> moves;
 
-/** Constructs an empty SudokuBoard. */
+  /** Constructs an empty SudokuBoard. */
   public SudokuBoard() {
     this(null);
   }
 
-/**
- * Primary SudokuBoard constructor.
- * Input may be null, which will create an empty board.
- * It also handles a string representing a board.
- *
- * @param board 81-character string representing a sudoku board.
- */
+  /**
+   * Primary SudokuBoard constructor.
+   * Input may be null, which will create an empty board.
+   * It also handles a string representing a board.
+   *
+   * @param board 81-character string representing a sudoku board.
+   */
   public SudokuBoard(String board) {
     cells = new SudokuSquare[9][9];
     moves = new ArrayDeque<Move>();
@@ -59,12 +59,11 @@ public class SudokuBoard {
     }
   }
 
-/**
- * Returns a new copy of the board.
- *
- * @param board the board to copy
- * @return copy of this board
- */
+  /**
+   * Returns a new copy of the board.
+   *
+   * @return copy of this board
+   */
   public SudokuBoard copyBoard() {
     SudokuBoard copy = new SudokuBoard(this.toString());
     for (int i = 1; i < 10; i++) {
@@ -81,25 +80,25 @@ public class SudokuBoard {
   }
 
 
-/**
- * Returns the value of the cell at (row, column).
- *
- * @param row row of the desired cell
- * @param col column of the desired cell
- */
+  /**
+   * Returns the value of the cell at (row, column).
+   *
+   * @param row row of the desired cell
+   * @param col column of the desired cell
+   */
   public int getValue(int row, int col) {
     validateRowCol(row);
     validateRowCol(col);
     return cells[row - 1][col - 1].value;
   }
 
-/**
- * Sets the value of a cell.
- *
- * @param row row of the desired cell
- * @param col column of the desired cell
- * @param value value to set the cell
- */
+  /**
+   * Sets the value of a cell.
+   *
+   * @param row row of the desired cell
+   * @param col column of the desired cell
+   * @param value value to set the cell
+   */
   public void setValue(int row, int col, int value) {
     validateRowCol(row);
     validateRowCol(col);
@@ -107,25 +106,25 @@ public class SudokuBoard {
     cells[row - 1][col - 1].setValue(value);
   }
 
-/**
- * Returns the pencils of the cell at (row, column).
- *
- * @param row row of the desired cell
- * @param col column of the desired cell
- */
+  /**
+   * Returns the pencils of the cell at (row, column).
+   *
+   * @param row row of the desired cell
+   * @param col column of the desired cell
+   */
   public ArrayList<Integer> getPencils(int row, int col) {
     validateRowCol(row);
     validateRowCol(col);
     return cells[row - 1][col - 1].pencils;
   }
 
-/**
- * Adds a specified pencil to the cell at (row, column).
- *
- * @param row row of the desired cell
- * @param col column of the desired cell
- * @param value value to add to pencils
- */
+  /**
+   * Adds a specified pencil to the cell at (row, column).
+   *
+   * @param row row of the desired cell
+   * @param col column of the desired cell
+   * @param value value to add to pencils
+   */
   public void addPencil(int row, int col, int value) {
     validateRowCol(row);
     validateRowCol(col);
@@ -133,13 +132,13 @@ public class SudokuBoard {
     cells[row - 1][col - 1].addPencil(value);
   }
 
-/**
- * Removes a specified pencil to the cell at (row, column).
- *
- * @param row row of the desired cell
- * @param col column of the desired cell
- * @param value value to remove from pencils
- */
+  /**
+   * Removes a specified pencil to the cell at (row, column).
+   *
+   * @param row row of the desired cell
+   * @param col column of the desired cell
+   * @param value value to remove from pencils
+   */
   public void removePencil(int row, int col, int value) {
     validateRowCol(row);
     validateRowCol(col);
@@ -147,12 +146,12 @@ public class SudokuBoard {
     cells[row - 1][col - 1].removePencil(value);
   }
 
-/**
- * Clear all pencils from the cell at (row, column).
- *
- * @param row row of the desired cell
- * @param col column of the desired cell
- */
+  /**
+   * Clear all pencils from the cell at (row, column).
+   *
+   * @param row row of the desired cell
+   * @param col column of the desired cell
+   */
   public void clearPencils(int row, int col) {
     validateRowCol(row);
     validateRowCol(col);
@@ -162,9 +161,9 @@ public class SudokuBoard {
     cells[row - 1][col - 1].clearPencils();
   }
 
-/**
- * Reverts the most recent change to the board.
- */
+  /**
+   * Reverts the most recent change to the board.
+   */
   public void undoMove() {
     try {
       reverseMove(moves.removeFirst());
@@ -173,11 +172,11 @@ public class SudokuBoard {
     }
   }
 
-/**
- * Returns the original board after reverting all moves.
- *
- * @return a copy of the board at its starting point
- */
+  /**
+   * Returns the original board after reverting all moves.
+   *
+   * @return a copy of the board at its starting point
+   */
   public SudokuBoard originalBoard() {
     SudokuBoard copy = new SudokuBoard();
     copy = copyBoard();
@@ -187,9 +186,9 @@ public class SudokuBoard {
     return copy;
   }
 
-/**
- * Examines a move and performs the reverse operation on the board.
- */
+  /**
+   * Examines a move and performs the reverse operation on the board.
+   */
   private void reverseMove(Move recentmove) {
     switch (recentmove.change) {
       case "+v":
@@ -209,11 +208,11 @@ public class SudokuBoard {
     }
   }
 
-/**
- * Returns the board as represented by an 81-character string.
- *
- * @return the 81-char string
- */
+  /**
+   * Returns the board as represented by an 81-character string.
+   *
+   * @return the 81-char string
+   */
   @Override
   public String toString() {
     String string = "";
@@ -225,11 +224,11 @@ public class SudokuBoard {
     return string;
   }
 
-/**
- * Returns the board as a string, formatted into a 9x9 grid.
- *
- * @return the formatted string
- */
+  /**
+   * Returns the board as a string, formatted into a 9x9 grid.
+   *
+   * @return the formatted string
+   */
   public String toPrettyString() {
     String string = "";
     for (int i = 0; i < 9; i++) {
@@ -250,12 +249,12 @@ public class SudokuBoard {
     return string;
   }
 
-/**
- * Compares one SudokuBoard to another.
- *
- * @param obj board to compare with
- * @return true if the same, false if not
- */
+  /**
+   * Compares one SudokuBoard to another.
+   *
+   * @param obj board to compare with
+   * @return true if the same, false if not
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -270,11 +269,10 @@ public class SudokuBoard {
     SudokuBoard compareboard = (SudokuBoard) obj;
     for (int i = 1; i < 10; i++) {
       for (int j = 1; j < 10; j++) {
-        if (this.getValue(i, j) == compareboard.getValue(i, j) &&
-            this.getPencils(i, j).equals(compareboard.getPencils(i,j))) {
+        if (this.getValue(i, j) == compareboard.getValue(i, j)
+            && this.getPencils(i, j).equals(compareboard.getPencils(i,j))) {
           continue;
-        }
-        else {
+        } else {
           return false;
         }
       }
@@ -282,11 +280,11 @@ public class SudokuBoard {
     return true;
   }
 
-/**
- * Generates a hashcode for the SudokuBoard.
- *
- * @return hashcode representing the board
- */
+  /**
+   * Generates a hashcode for the SudokuBoard.
+   *
+   * @return hashcode representing the board
+   */
   @Override
   public int hashCode() {
     int result = 1;
@@ -305,12 +303,12 @@ public class SudokuBoard {
     return result;
   }
 
-/**
- * Returns the specified row as an array of ints.
- *
- * @param row row number to return from 1-9
- * @return the array of square values in specified row
- */
+  /**
+   * Returns the specified row as an array of ints.
+   *
+   * @param row row number to return from 1-9
+   * @return the array of square values in specified row
+   */
   public int[] getRow(int row) {
     validateRowCol(row);
     int[] newrow = new int[9];
@@ -320,12 +318,12 @@ public class SudokuBoard {
     return newrow;
   }
 
-/**
- * Returns the specified column as an array of ints.
- *
- * @param col column number to return from 1-9
- * @return the array of square values in specified column
- */
+  /**
+   * Returns the specified column as an array of ints.
+   *
+   * @param col column number to return from 1-9
+   * @return the array of square values in specified column
+   */
   public int[] getCol(int col) {
     validateRowCol(col);
     int[] newcol = new int[9];
@@ -335,16 +333,17 @@ public class SudokuBoard {
     return newcol;
   }
 
-/**
- * Returns the region as an array of ints.
- * Regions are the nine 3x3 subgrids on the board numbered as follows:
- * 1 2 3
- * 4 5 6
- * 7 8 9
- *
- * @param region region number to return (1-9)
- * @return the array of squares in specified region
- */
+  /**
+   * Returns the region as an array of ints.
+   * Regions are the nine 3x3 subgrids on the board numbered as follows:
+   * 1 2 3
+   * 4 5 6
+   * 7 8 9
+   *
+   * @param row row of the cell
+   * @param col column of the cell
+   * @return the array of squares in specified region
+   */
   public int[] getReg(int row, int col) {
     validateRowCol(row);
     validateRowCol(col);
@@ -362,26 +361,26 @@ public class SudokuBoard {
     return newreg;
   }
 
-/**
- * Returns the region as an array of ints.
- * This method allows specifying the reg (1-9) as opposed to
- * a row and column specification.
- *
- * @param reg the region (1-9) to return
- */
+  /**
+   * Returns the region as an array of ints.
+   * This method allows specifying the reg (1-9) as opposed to
+   * a row and column specification.
+   *
+   * @param reg the region (1-9) to return
+   */
   public int[] getReg(int reg) {
     int[] startingrow = {1, 1, 1, 4, 4, 4, 7, 7, 7};
     int[] startingcol = {1, 4, 7, 1, 4, 7, 1, 4, 7};
     return getReg(startingrow[reg - 1], startingcol[reg - 1]);
   }
 
-/**
- * Returns the region number for the cell at (row, col)
- *
- * @param row row of specified cell
- * @param col column of specified cell
- * @return the region number corresponding to the cell
- */
+  /**
+   * Returns the region number for the cell at (row, col).
+   *
+   * @param row row of specified cell
+   * @param col column of specified cell
+   * @return the region number corresponding to the cell
+   */
   public static int findReg(int row, int col) {
     if (row < 4 && col < 4) {
       return 1;
@@ -407,27 +406,27 @@ public class SudokuBoard {
     if (col < 7) {
       return 8;
     }
-      return 9;
+    return 9;
   }
 
-/**
- * Throws exceptions for a bad row or column input
- *
- * @param index row or column to check
- */
+  /**
+   * Throws exceptions for a bad row or column input.
+   *
+   * @param index row or column to check
+   */
   private void validateRowCol(int index) {
-      if (index < 1 || index > 9) {
-        throw new IllegalArgumentException(
-            "Rows and columns must be from 1 to 9.");
-      }
+    if (index < 1 || index > 9) {
+      throw new IllegalArgumentException(
+          "Rows and columns must be from 1 to 9.");
     }
+  }
 
-/**
- * Object representing a change made to the board.
- * Each move will contain information on a change made to the board.
- * Moves will mainly be used in keeping a record of how a board
- * transitioned from one point to another (from unsolved to solved).
- */
+  /**
+   * Object representing a change made to the board.
+   * Each move will contain information on a change made to the board.
+   * Moves will mainly be used in keeping a record of how a board
+   * transitioned from one point to another (from unsolved to solved).
+   */
   class Move {
     private int row;
     private int col;
@@ -442,102 +441,104 @@ public class SudokuBoard {
     }
   }
 
-/**
- * Basic sudoku board building block.
- * SudokuSquare is the class of objects that will make up a sudoku board.
- * Each square will be able to hold a single value as well as multiple
- * penciled values.
- */
+  /**
+   * Basic sudoku board building block.
+   * SudokuSquare is the class of objects that will make up a sudoku board.
+   * Each square will be able to hold a single value as well as multiple
+   * penciled values.
+   */
   class SudokuSquare {
     private ArrayList<Integer> pencils;
     private int value;
-  /**
-   * Constructs an empty SudokuSquare.
-   * Value set to 0 by default.
-   */
+
+    /**
+     * Constructs an empty SudokuSquare.
+     * Value set to 0 by default.
+     */
     private SudokuSquare() {
       this(0);
     }
 
-  /**
-   * Constructs a SudokuSquare with the specified value.
-   *
-   * @param value value to create the square with
-   */
+    /**
+     * Constructs a SudokuSquare with the specified value.
+     *
+     * @param value value to create the square with
+     */
     private SudokuSquare(int value) {
       pencils = new ArrayList<Integer>();
       validateValue(value);
       this.value = value;
     }
 
-  /**
-   * Sets the square value.
-   * Value must be between 1 and 9 inclusive.
-   *
-   * @param value desired value of square
-   */
+    /**
+     * Sets the square value.
+     * Value must be between 1 and 9 inclusive.
+     *
+     * @param value desired value of square
+     */
     private void setValue(int value) {
       validateValue(value);
       this.value = value;
     }
 
-  /**
-   * Adds a penciled value.
-   *
-   * @param value value to pencil in
-   */
+    /**
+     * Adds a penciled value.
+     *
+     * @param value value to pencil in
+     */
     private void addPencil(int value) {
       validateValue(value);
       pencils.add(value);
       Collections.sort(pencils);
     }
 
-  /**
-   * Removes a penciled value.
-   *
-   * @param value value to unpencil
-   */
+    /**
+     * Removes a penciled value.
+     *
+     * @param value value to unpencil
+     */
     private void removePencil(int value) {
       validateValue(value);
       pencils.remove(new Integer(value));
     }
 
-  /** Removes all penciled values. */
+    /** Removes all penciled values. */
     private void clearPencils() {
       pencils.clear();
     }
 
-  /**
-   * Returns (empty/full) state of square.
-   *
-   * @return true if a value other than 0, false if 0.
-   */
+    /**
+     * Returns (empty/full) state of square.
+     *
+     * @return true if a value other than 0, false if 0.
+     */
     private boolean isFull() {
       return (value != 0);
     }
 
-  /**
-   * Allows SudokuSquares to be printable with println.
-   *
-   * @return the string representation of the square's value
-   */
+    /**
+     * Allows SudokuSquares to be printable with println.
+     *
+     * @return the string representation of the square's value
+     */
     @Override
     public String toString() {
       return Integer.toString(value);
     }
 
-  /**
-   * Throws exceptions for a bad input.
-   *
-   * @param value value to check
-   */
+    /**
+     * Throws exceptions for a bad input.
+     *
+     * @param value value to check
+     */
     private void validateValue(int value) {
       if (value < 0 || value > 9) {
         throw new IllegalArgumentException("Value must be between 0 and 9.");
       }
     }
   }
-/** Method for quick testing. */
+
+  /** Method for quick testing. */
   public static void main(String[] args) {
     try {
       //accepts a board string as a command line argument
@@ -546,9 +547,8 @@ public class SudokuBoard {
       SudokuBoard sb = new SudokuBoard(boardstring);
     } catch (FileNotFoundException e) {
       System.out.println("File not found");
-      }
-      catch (IOException e) {
+    } catch (IOException e) {
       System.out.println("IO Exception.");
-      }
+    }
   }
 }
